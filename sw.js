d842b67478e52842570d6a71393ba0eca22789d2
tirta kencana <afhpartnertirtakencana@gmail.com>
2026-07-02 23:58:45 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "pos-umkm-v1";
+const CORE_ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./assets/styles.css",
+  "./assets/db.js",
+  "./assets/ui.js",
+  "./assets/app.js",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            try { cache.put(event.request, copy); } catch (e) {}
+          });
+          return res;
+        })
+        .catch(() => cached);
+    })
+  );
+});
